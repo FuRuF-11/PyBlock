499e40a2163ca705cddc083c8bad75d548ff9c86
furuf <2841462631@qq.com>
2024-01-10 23:13:38 +0800

    new file:   Transformer/attention.py 	new file:   Transformer/decoder.py 	modified:   Transformer/encoder.py 	new file:   Transformer/feedforward.py 	new file:   Transformer/position.py 	new file:   Transformer/transformer.py

--- Transformer/encoder.py
+++ Transformer/encoder.py
@@ -1,12 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-class AttentionBlock(nn.Module):
-    def __init__(self,X) -> None:
-        super(AttentionBlock,self).__init__()
-        self.W=nn.Embedding()
-
+    
 class TransformerEncoderBlock(nn.Module):
     def __init__(self,in_channal,out_channal,head):
         super(TransformerEncoderBlock,self).__init__
+
+class Encoder(nn.Module):
+    def __init__(self) -> None:
+        super(Encoder,self).__init__()
+
+    def forward(self,X):
+        return X
