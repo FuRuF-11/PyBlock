c659f8b44c915563793af15255e5fb7e187167a1
furuf <2841462631@qq.com>
2024-01-15 00:09:50 +0800

    position encoder

---
+++ Transformer/position.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+'''
+all kinds of position encoder 
+'''
+
+class cosinPosition():
+    def __init__(self) -> None:
+        pass
