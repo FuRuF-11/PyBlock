2902345f22eb161e916a90b89c1f69fe7b2c34a0
furuf <2841462631@qq.com>
2024-01-17 22:43:52 +0800

    modified

--- Transformer/encoder.py
+++ Transformer/encoder.py
@@ -1,11 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-    
-class TransformerEncoderBlock(nn.Module):
-    def __init__(self,in_channal,out_channal,head):
-        super(TransformerEncoderBlock,self).__init__
+from attention import CasualAttention
+from feedforward import MLP
+from position import CosinPosition
 
+class EncoderBlock(nn.Module):
+    def __init__(self,d_model,sentence_length,head=8,dropout=0.1):
+        super(EncoderBlock,self).__init__
+
+    def forward(self,X):
+        return X    
 class Encoder(nn.Module):
     def __init__(self) -> None:
         super(Encoder,self).__init__()
