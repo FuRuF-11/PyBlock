06591d888e5c57eea9aa91db431775ac6697de66
furuf <2841462631@qq.com>
2024-01-19 18:26:43 +0800

    modified:   Transformer/encoder.py 	new file:   Transformer/readme.md

--- Transformer/encoder.py
+++ Transformer/encoder.py
@@ -6,10 +6,8 @@ from attention import SelfAttention
 from feedforward import MLP
 from position import CosinPosition
 
-
 def cloneLayers(layer,n):
-    return nn.ModuleList(copy.deepcopy(layer))
-
+    return nn.ModuleList([copy.deepcopy(layer) for _ in range(n)])
 
 class EncoderBlock(nn.Module):
     def __init__(self,d_model,head=8,dropout=0.1):
@@ -19,14 +17,22 @@ class EncoderBlock(nn.Module):
         self.attention=SelfAttention(d_model,head,dropout)
         self.feedforward=MLP(d_model,dropout)
 
-    def forward(self,X):
-        X=X+self.attention(self.Norm1(X))
+    def forward(self,X,mask=None):
+        X=X+self.attention(self.Norm1(X),mask)
         X=X+self.feedforward(self.Norm2(X))
         return X
     
 class Encoder(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,d_model,layer_size=6,head=8,dropout=0.1,max_length=1000) -> None:
         super(Encoder,self).__init__()
+        self.N=layer_size
+        self.position=CosinPosition(d_model,max_length,dropout)
+        self.layers=cloneLayers(EncoderBlock(d_model,head,dropout),self.N)
+        self.Norm=nn.LayerNorm(d_model)
 
-    def forward(self,X):
+    def forward(self,X,mask=None):
+        X=self.position(X)
+        for i in range(self.N):
+            X=self.layers[i](X,mask)
+        X=self.Norm(X)
         return X
---
+++ Transformer/readme.md
@@ -0,0 +1,2 @@
+In this implementation, I do not set the embedding block which you may see in others` implementation.  
+If you want to use the embedding, you need to use word2vec.
