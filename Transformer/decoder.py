ded540c35c970bdbc06fa9a45671ffb03ba3d551
furuf <2841462631@qq.com>
2024-01-19 23:00:40 +0800

    modified:   Transformer/decoder.py 	new file:   Transformer/embedding.py 	deleted:    Transformer/readme.md

--- Transformer/decoder.py
+++ Transformer/decoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import copy
-from attention import SelfAttention
+from attention import SelfAttention,CasualSelfAttention,MultiHeadAttention
 from feedforward import MLP
 from position import CosinPosition,RnnPosition
 
@@ -10,8 +10,14 @@ def cloneLayers(layer,n):
     return nn.ModuleList([copy.deepcopy(layer) for _ in range(n)])
 
 class DecoderBlock(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,d_model,head=8,max_length=2000,dropout=0.1) -> None:
         super(DecoderBlock,self).__init__()
+        self.position=CosinPosition(d_model,max_length,dropout)
+        self.attention1=SelfAttention(d_model,head,dropout)
+        self.attention2
+
+    def forward(self,X,en_output):
+        return X
 
 class Decoder(nn.Module):
     def __init__(self) -> None:
--- Transformer/readme.md
+++
@@ -1,3 +0,0 @@
-In this implementation, I do not set the embedding block which you may see in else implementation.
-  
-If you want to use the embedding, I suppose you  use word2vec.
