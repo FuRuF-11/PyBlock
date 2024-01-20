52caa41c2cb405ad18d0cace7ad36ee24393eacf
furuf <2841462631@qq.com>
2024-01-20 10:33:53 +0800

    modified:   Transformer/decoder.py

--- Transformer/decoder.py
+++ Transformer/decoder.py
@@ -26,6 +26,24 @@ class Decoder(nn.Module):
     def __init__(self) -> None:
         super(Decoder,self).__init__()
 
+    def sourceMask(src):
+        '''
+        the length of source sentences are not always same, this is not good for computation.
+        to make sequences have the same length, we need to align sentences from left.
+        so a real data batch may look like this, in which 'N' means None
+        [C,C.C,C,C,N,N]
+        [V,V.V,V,N,N,N]
+        [K,K,K,K,K,N,N]
+        we need to mask N to prevent attention mechanisms from noticing them.
+        and that is what this function for
+        '''
+        src_mask=[]
+        return src_mask
+
+
+    def forward(self,X):
+        return X
+
 class GPT(nn.Module):
     def __init__(self) -> None:
         super(GPT,self).__init__()
