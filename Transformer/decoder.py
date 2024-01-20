27527c52ee1b7d426f2cf6b165e85e56cf6b1876
furuf <2841462631@qq.com>
2024-01-20 17:32:22 +0800

    finish decoder

--- Transformer/decoder.py
+++ Transformer/decoder.py
@@ -29,24 +29,35 @@ def sourceMask(src,pad=0):
         return src_mask
 
 class DecoderBlock(nn.Module):
-    def __init__(self,d_model,head=8,max_length=2000,dropout=0.1) -> None:
+    def __init__(self,d_model,head=8,dropout=0.1) -> None:
         super(DecoderBlock,self).__init__()
-        self.position=CosinPosition(d_model,max_length,dropout)
         self.attention1=CasualSelfAttention(d_model,head,dropout)
         self.attention2=MultiHeadAttention(d_model,head,dropout)
         self.feedforward=MLP(d_model,dropout)
         self.Norm1=nn.LayerNorm(d_model)
         self.Norm2=nn.LayerNorm(d_model)
+        self.Norm3=nn.LayerNorm(d_model)
 
     def forward(self,X,en_output,src_mask=None):
-        
+        X=X+self.attention1(self.Norm1(X))
+        x=self.Norm2(X)
+        X=X+self.attention2(X,en_output,en_output,src_mask)
+        X=X+self.feedforward(self.Norm3(X))
         return X
 
 class Decoder(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,d_model,layer_size=6,head=8,dropout=0.1,max_length=2000) -> None:
         super(Decoder,self).__init__()
+        self.N=layer_size
+        self.position=CosinPosition(d_model,max_length,dropout)
+        self.layers=cloneLayers(DecoderBlock(d_model,head,dropout),layer_size)
+        self.Norm=nn.LayerNorm(d_model)
 
-    def forward(self,X):
+    def forward(self,X,en_output,src_mask=None):
+        X=X+self.position(X)
+        for i in range(self.N):
+            X=self.layers[i](X,en_output,src_mask)
+        X=self.Norm(X)
         return X
 
 class GPT(nn.Module):
