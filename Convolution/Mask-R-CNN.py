0ec599822765b43edfe43efc9c857817ac2b05ae
furuf <2841462631@qq.com>
2024-01-30 23:28:23 +0800

    modified:   Convolution/Mask-R-CNN.py 	modified:   Convolution/WaveNet.py

--- Convolution/Mask-R-CNN.py
+++ Convolution/Mask-R-CNN.py
@@ -2,3 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+# class backbone()
+
+# class Mask()
+
+# class RegionProposalNet()
+
+class MaskRCNN(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self,X):
+        return 
