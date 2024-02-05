1306a94858adbb72bd5928de4d41cc4f1826291a
furuf <2841462631@qq.com>
2024-02-05 13:10:54 +0800

    modified:   Convolution/FCN.py

--- Convolution/FCN.py
+++ Convolution/FCN.py
@@ -18,7 +18,7 @@ class VGG16net(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.model=torchvision.models.vgg16(pretrained=True)
-        
+
 
 
 
@@ -53,71 +53,50 @@ class FCN(nn.Module):
         return output
         
 
-# set layer name set of resnet18
-# conv1
-# bn1
-# relu
-# maxpool
-# layer1
-# layer1.0
-# layer1.0.conv1
-# layer1.0.bn1
-# layer1.0.relu
-# layer1.0.conv2
-# layer1.0.bn2
-# layer1.1
-# layer1.1.conv1
-# layer1.1.bn1
-# layer1.1.relu
-# layer1.1.conv2
-# layer1.1.bn2
-# layer2
-# layer2.0
-# layer2.0.conv1
-# layer2.0.bn1
-# layer2.0.relu
-# layer2.0.conv2
-# layer2.0.bn2
-# layer2.0.downsample
-# layer2.0.downsample.0
-# layer2.0.downsample.1
-# layer2.1
-# layer2.1.conv1
-# layer2.1.bn1
-# layer2.1.relu
-# layer2.1.conv2
-# layer2.1.bn2
-# layer3
-# layer3.0
-# layer3.0.conv1
-# layer3.0.bn1
-# layer3.0.relu
-# layer3.0.conv2
-# layer3.0.bn2
-# layer3.0.downsample
-# layer3.0.downsample.0
-# layer3.0.downsample.1
-# layer3.1
-# layer3.1.conv1
-# layer3.1.bn1
-# layer3.1.relu
-# layer3.1.conv2
-# layer3.1.bn2
-# layer4
-# layer4.0
-# layer4.0.conv1
-# layer4.0.bn1
-# layer4.0.relu
-# layer4.0.conv2
-# layer4.0.bn2
-# layer4.0.downsample
-# layer4.0.downsample.0
-# layer4.0.downsample.1
-# layer4.1
-# layer4.1.conv1
-# layer4.1.bn1
-# layer4.1.relu
-# layer4.1.conv2
-# layer4.1.bn2
-# avgpool
-# fc
+# set layer name set of VGG16net
+# ['', 'features', 'features.0', 'features.1', 'features.2', 'features.3', 'features.4', 'features.5', 'features.6', 'features.7', 'features.8', 'features.9', 'features.10', 'features.11', 'features.12', 'features.13', 'features.14', 'features.15', 'features.16', 'features.17', 'features.18', 'features.19', 'features.20', 'features.21', 'features.22', 'features.23', 'features.24', 'features.25', 'features.26', 'features.27', 'features.28', 'features.29', 'features.30', 'avgpool', 'classifier', 'classifier.0', 'classifier.1', 'classifier.2', 'classifier.3', 'classifier.4', 'classifier.5', 'classifier.6']
+# VGG(
+#   (features): Sequential(
+#     (0): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (1): ReLU(inplace=True)
+#     (2): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (3): ReLU(inplace=True)
+#     (4): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (5): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (6): ReLU(inplace=True)
+#     (7): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (8): ReLU(inplace=True)
+#     (9): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (10): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (11): ReLU(inplace=True)
+#     (12): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (13): ReLU(inplace=True)
+#     (14): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (15): ReLU(inplace=True)
+#     (16): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (17): Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (18): ReLU(inplace=True)
+#     (19): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (20): ReLU(inplace=True)
+#     (21): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (22): ReLU(inplace=True)
+#     (23): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (24): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (25): ReLU(inplace=True)
+#     (26): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (27): ReLU(inplace=True)
+#     (28): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (29): ReLU(inplace=True)
+#     (30): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#   )
+#   (avgpool): AdaptiveAvgPool2d(output_size=(7, 7))
+#   (classifier): Sequential(
+#     (0): Linear(in_features=25088, out_features=4096, bias=True)
+#     (1): ReLU(inplace=True)
+#     (2): Dropout(p=0.5, inplace=False)
+#     (3): Linear(in_features=4096, out_features=4096, bias=True)
+#     (4): ReLU(inplace=True)
+#     (5): Dropout(p=0.5, inplace=False)
+#     (6): Linear(in_features=4096, out_features=1000, bias=True)
+#   )
+# )
