fc3d3fc2fe43031a84d97ff4c394e788132fa5aa
furuf <2841462631@qq.com>
2024-02-09 10:09:43 +0800

    modified:   Recurrent/NormalRNNs.py

--- Recurrent/NormalRNNs.py
+++ Recurrent/NormalRNNs.py
@@ -9,6 +9,9 @@ class RNN(nn.Module):
         super().__init__()
         self.d_model=d_model
         self.hidden_size=hidden_size
+        self.layers=num_layer
+        self.bid=bid
+        
         self.rnn=nn.RNN(d_model,hidden_size,num_layer,\
                         batch_first=True,dropout=dropout,bidirectional=bid)
         self.classfier=nn.Sequential(
@@ -18,8 +21,13 @@ class RNN(nn.Module):
         )
 
     def forward(self,X):
-        h0=torch.zeros()
-        return 
+        if(self.bid==True):
+            h0=torch.zeros(X.size(0),2*self.num_layer,self.hidden_size)
+        else:
+            h0=torch.zeros(X.size(0),self.num_layer,self.hidden_size)
+        X,h=self.rnn(X,h0)
+        out=self.classfier(X[:,-1,:])
+        return out
     
 
 
