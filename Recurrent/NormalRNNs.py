6979b17bccc49a7dcf2dfc45b1792d857251f9bf
furuf <2841462631@qq.com>
2024-02-09 10:34:05 +0800

    modified:   Recurrent/NormalRNNs.py

--- Recurrent/NormalRNNs.py
+++ Recurrent/NormalRNNs.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
-
 class RNN(nn.Module):
     def __init__(self,d_model,hidden_size,num_layer,output_size,dropout=0.1,bid=False) -> None:
         super().__init__()
@@ -25,25 +24,57 @@ class RNN(nn.Module):
             h0=torch.zeros(X.size(0),2*self.num_layer,self.hidden_size)
         else:
             h0=torch.zeros(X.size(0),self.num_layer,self.hidden_size)
-        X,h=self.rnn(X,h0)
+        X,h_n=self.rnn(X,h0)
         out=self.classfier(X[:,-1,:])
         return out
     
-
-
 class GRU(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,d_model,hidden_size,num_layer,output_size,dropout=0.1,bid=False) -> None:
         super().__init__()
+        self.d_model=d_model
+        self.hidden_size=hidden_size
+        self.layers=num_layer
+        self.bid=bid
+        
+        self.rnn=nn.GRU(d_model,hidden_size,num_layer,\
+                        batch_first=True,dropout=dropout,bidirectional=bid)
+        self.classfier=nn.Sequential(
+            nn.Linear(hidden_size,output_size),
+            nn.ReLU(),
+            nn.Softmax(output_size)
+        )
 
     def forward(self,X):
-        return 
-    
+        if(self.bid==True):
+            h0=torch.zeros(X.size(0),2*self.num_layer,self.hidden_size)
+        else:
+            h0=torch.zeros(X.size(0),self.num_layer,self.hidden_size)
+        X,h_n=self.rnn(X,h0)
+        out=self.classfier(X[:,-1,:])
+        return out
 
 
 class LSTM(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,d_model,hidden_size,num_layer,output_size,dropout=0.1,bid=False) -> None:
         super().__init__()
+        self.d_model=d_model
+        self.hidden_size=hidden_size
+        self.layers=num_layer
+        self.bid=bid
+        
+        self.rnn=nn.LSTM(d_model,hidden_size,num_layer,\
+                        batch_first=True,dropout=dropout,bidirectional=bid)
+        self.classfier=nn.Sequential(
+            nn.Linear(hidden_size,output_size),
+            nn.ReLU(),
+            nn.Softmax(output_size)
+        )
 
     def forward(self,X):
-        return 
-    
+        if(self.bid==True):
+            h0=torch.zeros(X.size(0),2*self.num_layer,self.hidden_size)
+        else:
+            h0=torch.zeros(X.size(0),self.num_layer,self.hidden_size)
+        X,h_n=self.rnn(X,h0)
+        out=self.classfier(X[:,-1,:])
+        return out
