700d6674393a09da51d99708b711a7cc2bf168c1
furuf <2841462631@qq.com>
2024-03-12 20:41:20 +0800

    modified:   Recurrent/NormalRNNs.py

--- Recurrent/NormalRNNs.py
+++ Recurrent/NormalRNNs.py
@@ -59,22 +59,24 @@ class LSTM(nn.Module):
         super().__init__()
         self.d_model=d_model
         self.hidden_size=hidden_size
-        self.layers=num_layer
+        self.layer=num_layer
         self.bid=bid
         
         self.rnn=nn.LSTM(d_model,hidden_size,num_layer,\
                         batch_first=True,dropout=dropout,bidirectional=bid)
-        self.classfier=nn.Sequential(
-            nn.Linear(hidden_size,output_size),
-            nn.ReLU(),
-            nn.Softmax(output_size)
-        )
+        # self.classfier=nn.Sequential(
+        #     nn.Linear(hidden_size,output_size),
+        #     nn.ReLU(),
+        #     nn.Softmax(output_size)
+        # )
 
     def forward(self,X):
         if(self.bid==True):
-            h0=torch.zeros(X.size(0),2*self.num_layer,self.hidden_size)
+            h0=torch.zeros(2*self.layer,X.size(0),self.hidden_size)
+            c0=torch.zeros(2*self.layer,X.size(0),self.hidden_size)
         else:
-            h0=torch.zeros(X.size(0),self.num_layer,self.hidden_size)
-        X,h_n=self.rnn(X,h0)
-        out=self.classfier(X[:,-1,:])
-        return out
+            h0=torch.zeros(self.layer,X.size(0),self.hidden_size)
+            c0=torch.zeros(self.layer,X.size(0),self.hidden_size)
+        X,(hn,cn)=self.rnn(X,(h0,c0))
+        # out=self.classfier(X[:,-1,:])
+        return X,hn,cn
