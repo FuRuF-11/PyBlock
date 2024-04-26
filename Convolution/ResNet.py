9e5457d7ad4712e4ec50c5ddeb88cd0773cde07b
furuf <2841462631@qq.com>
2024-04-26 20:36:29 +0800

    modified:   Convolution/ResNet.py

--- Convolution/ResNet.py
+++ Convolution/ResNet.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
+class ResBlock(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+
 class ResNet(nn.Module):
     def __init__(self,in_channel,hidden_channel,output_size,layers):
         super().__init__()
@@ -10,5 +15,7 @@ class ResNet(nn.Module):
         self.hidden_channel=hidden_channel
         self.output_size=output_size
         self.layers=layers
+
+
     def forward(self,X):
         return
