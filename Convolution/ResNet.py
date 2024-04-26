2ca88add8ed4504077ac695b371853b5a3eb89e4
furuf <2841462631@qq.com>
2024-04-26 10:49:38 +0800

    new file:   Convolution/ResNet.py

---
+++ Convolution/ResNet.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+class ResNet(nn.Module):
+    def __init__(self,in_channel,hidden_channel,output,layer):
+        super().__init__()
+
+    def forward(self,X):
+        return
