6422e0578060e55b4fbf82c733ed6443d7442b29
furuf <2841462631@qq.com>
2024-05-02 20:46:27 +0800

    modified:   Convolution/ResNet.py 	new file:   Convolution/__init__.py

--- Convolution/ResNet.py
+++ Convolution/ResNet.py
@@ -3,13 +3,68 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
-class ResBlock(nn.Module):
-    def __init__(self,in_channel,out_channel,kernel_size=3) -> None:
-        super().__init__()
-        self.conv1=nn.Conv2d(in_channels=in_channel,out_channels=out_channel,kernel_size=kernel_size)
+# 网络参数设置
+'''
+18
+{
+    conv1: 7x7 64 stride 2,
+           3x3 maxpool stride 2,
+    conv2: [3x3 64, 3x3 64]x2,
+    conv3: [3x3 128, 3x3 128]x2,
+    conv4: [3x3 256, 3x3 256]x2,
+    conv5: [3x3 512, 3x3 512]x2,
+    average pool: 1000-d fc, softmax
+}'''
 
-    def forward(self,X,a=1.0):
-        return 
+'''
+34
+{
+    conv1: 7x7 64 stride 2,
+           3x3 maxpool stride 2,
+    conv2: [3x3 64, 3x3 64]x3,
+    conv3: [3x3 128, 3x3 128]x4,
+    conv4: [3x3 256, 3x3 256]x6,
+    conv5: [3x3 512, 3x3 512]x3,
+    average pool: 1000-d fc, softmax
+}'''
+
+'''
+50
+{
+    conv1: 7x7 64 stride 2,
+           3x3 maxpool stride 2,
+    conv2: [1x1 64, 3x3 64, 1x1 256]x3,
+    conv3: [1x1 128, 3x3 128, 1x1 512]x4,
+    conv4: [1x1 256, 3x3 256, 1x1 1024]x6,
+    conv5: [1x1 512, 3x3 512, 1x1 2048]x3,
+    average pool: 1000-d fc, softmax
+}'''
+
+'''
+101
+{
+    conv1: 7x7 64 stride 2,
+           3x3 maxpool stride 2,
+    conv2: [1x1 64, 3x3 64, 1x1 256]x3,
+    conv3: [1x1 128, 3x3 128, 1x1 512]x4,
+    conv4: [1x1 256, 3x3 256, 1x1 1024]x23,
+    conv5: [1x1 512, 3x3 512, 1x1 2048]x3,
+    average pool: 1000-d fc, softmax
+}'''
+
+'''
+152
+{
+    conv1: 7x7 64 stride 2,
+           3x3 maxpool stride 2,
+    conv2: [1x1 64, 3x3 64, 1x1 256]x3,
+    conv3: [1x1 128, 3x3 128, 1x1 512]x8,
+    conv4: [1x1 256, 3x3 256, 1x1 1024]x36,
+    conv5: [1x1 512, 3x3 512, 1x1 2048]x3,
+    average pool: 1000-d fc, softmax
+}'''
+
+
 class ResNet(nn.Module):
     def __init__(self,in_channel,hidden_channel,output_size,layers):
         super().__init__()
@@ -17,8 +72,11 @@ class ResNet(nn.Module):
         self.hidden_channel=hidden_channel
         self.output_size=output_size
         self.layers=layers
-        self.conv0=nn.Conv2d(in_channels=in_channel,)
-    
+        self.conv0=nn.Sequential(
+            nn.Conv2d(in_channels=in_channel,out_channels=64,kernel_size=7,stride=2),
+            nn.MaxPool2d(kernel_size=3,stride=2)
+        )
+        
 
     def forward(self,X):
         return
