eeaf9af17b2560f8e2e2549c3826b4c1c1756af6
furuf <2841462631@qq.com>
2024-05-03 23:40:03 +0800

    modified:   Convolution/ResNet.py

--- Convolution/ResNet.py
+++ Convolution/ResNet.py
@@ -5,8 +5,7 @@ import math
 
 # 网络参数设置
 '''
-18
-{
+18 {
     conv1: 7x7 64 stride 2,
            3x3 maxpool stride 2,
     conv2: [3x3 64, 3x3 64]x2,
@@ -14,11 +13,8 @@ import math
     conv4: [3x3 256, 3x3 256]x2,
     conv5: [3x3 512, 3x3 512]x2,
     average pool: 1000-d fc, softmax
-}'''
-
-'''
-34
-{
+}
+34 {
     conv1: 7x7 64 stride 2,
            3x3 maxpool stride 2,
     conv2: [3x3 64, 3x3 64]x3,
@@ -26,11 +22,8 @@ import math
     conv4: [3x3 256, 3x3 256]x6,
     conv5: [3x3 512, 3x3 512]x3,
     average pool: 1000-d fc, softmax
-}'''
-
-'''
-50
-{
+}
+50 {
     conv1: 7x7 64 stride 2,
            3x3 maxpool stride 2,
     conv2: [1x1 64, 3x3 64, 1x1 256]x3,
@@ -38,11 +31,8 @@ import math
     conv4: [1x1 256, 3x3 256, 1x1 1024]x6,
     conv5: [1x1 512, 3x3 512, 1x1 2048]x3,
     average pool: 1000-d fc, softmax
-}'''
-
-'''
-101
-{
+}
+101 {
     conv1: 7x7 64 stride 2,
            3x3 maxpool stride 2,
     conv2: [1x1 64, 3x3 64, 1x1 256]x3,
@@ -50,11 +40,8 @@ import math
     conv4: [1x1 256, 3x3 256, 1x1 1024]x23,
     conv5: [1x1 512, 3x3 512, 1x1 2048]x3,
     average pool: 1000-d fc, softmax
-}'''
-
-'''
-152
-{
+}
+152 {
     conv1: 7x7 64 stride 2,
            3x3 maxpool stride 2,
     conv2: [1x1 64, 3x3 64, 1x1 256]x3,
@@ -62,21 +49,26 @@ import math
     conv4: [1x1 256, 3x3 256, 1x1 1024]x36,
     conv5: [1x1 512, 3x3 512, 1x1 2048]x3,
     average pool: 1000-d fc, softmax
-}'''
+}
+'''
 
 
-class ResNet(nn.Module):
-    def __init__(self,in_channel,hidden_channel,output_size,layers):
+
+class ResNet18(nn.Module):
+    def __init__(self,output_size,in_channel):
         super().__init__()
-        self.in_channel=in_channel
-        self.hidden_channel=hidden_channel
         self.output_size=output_size
-        self.layers=layers
         self.conv0=nn.Sequential(
             nn.Conv2d(in_channels=in_channel,out_channels=64,kernel_size=7,stride=2),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=3,stride=2)
         )
-        
+        self.conv2=nn.Sequential(
+            nn.Conv2d(),
+            nn.ReLU(),
+            nn.Conv2d(),
+            nn.ReLU(),
+        )
 
     def forward(self,X):
         return
